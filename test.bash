cc4d74266c8105248ecc63309ddcfad7e877d5c5
Takashi Iwasaki <s24c1018lq@s.chibakoudai.jp>
2025-10-30 13:50:55 +0900

    add a stlenge cases

---
+++ test.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2025 Takashi Iwasaki
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+	echo ${1}line wrong
+	res=1
+}
+
+res=0
+
+out=$(seq 5 | ./plus)
+
+[ "${out}" = 15.0 ] || ng "$LINENO"
+
+
+out=$(echo a | ./plus)
+
+[ "$?" = 1 ]        || ng "$LINENO"
+[ "${out}" = "" ]   || ng "$LINENO"
+
+out=$(echo | ./plus)
+
+[ "$?" = 1 ]        || ng "$LINENO"
+[ "${out}" = "" ]   || ng "$LINENO"
+
+[ "${res}" = 0 ]  && echo OK
+
+exit $res
